d74cbde4fe0e87e75c101d51d9e4c91e933408f0
yarwest <yarno1998@gmail.com>
2016-08-01 01:00:59 +0200

    Design 1 completely functional and stylised. Base img folder added
    Design 1 is complete with JS and CSS,matches design fully.
    Base img folder added to root, to be used by all designs for example
    pictures.

---
+++ Design 1/js/main.js
@@ -0,0 +1,20 @@
+var photos = ['1.jpg', '2.jpg'],
+    nPhotos = photos.length,
+    currentPhoto = 0;
+
+function change(direction) {
+    if(direction === 'left') {
+        if(currentPhoto === 0) {
+            currentPhoto = nPhotos-1;
+        } else {
+            currentPhoto--;
+        };
+    } else if(direction === 'right') {
+        if(currentPhoto === nPhotos-1) {
+            currentPhoto = 0;
+        } else {
+            currentPhoto++;
+        };
+    }
+    document.getElementById('img').src='../img/' + photos[currentPhoto];
+}
