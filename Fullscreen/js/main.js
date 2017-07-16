ced0bad7d4e1b17ee6a9a0c684e3e446c3799889
yarwest <yarnoboelens@gmail.com>
2017-07-16 19:19:39 +0200

    Created a fullscreen only version.

---
+++ Fullscreen/js/main.js
@@ -0,0 +1,10 @@
+/* Opens overlay by setting display to 'block', also calls assign function for overlay-img*/
+function openOverlay() {
+    document.getElementById('overlay').style.display="block";
+    assign('overlay-img');
+}
+
+/* Closes overlay by setting display to 'none' */
+function closeOverlay() {
+    document.getElementById('overlay').style.display="none";
+}
